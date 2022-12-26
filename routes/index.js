c6e5e0f89e9d64b012e6c932bbfd9c78c0ff2d83
Radha060301 <purnamasycandra060301@gmail.com>
2022-12-26 15:51:51 +0700

    REST API

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,64 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var data = [{
+  nama: "Agus", umur: "28"
+}];
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({msg: "Berhasil"});
 });
+
+router.get('/data', function(req, res, next) {
+  res.json({identitas: data});
+});
+
+router.post('/data', function(req, res, next) {
+  const { nama, umur } = req.body;
+
+  if (String(nama) && String(umur)) {
+    try {
+      data.push({nama: String(nama), umur: String(umur)});
+      res.json({msg: "Berhasil"});
+    } catch (error) {
+      res.json({msg: "Gagal"});
+    }
+  } else {
+    res.json({msg: "Gagal"});
+  }
+});
+
+router.put('/data', function(req, res, next) {
+  const { id, nama, umur } = req.body;
+
+  if (String(id) && String(nama) && String(umur)) {
+    if (id >= 0 && id < data.length) {
+      data[id] = {nama: String(nama), umur: String(umur)};
+      res.json({msg: "Berhasil"});
+    } else {
+      res.json({msg: "Gagal"});
+    }
+  } else {
+    res.json({msg: "Gagal"});
+  }
+});
+
+router.delete('/data', function(req, res, next) {
+  const { id } = req.body;
+
+  if (String(id)) {
+    if (id >= 0 && id < data.length) {
+      for (const key in data[id]) {
+        delete data[id][key];
+      }
+      delete data[id];
+      res.json({msg: "Berhasil"});
+    } else {
+      res.json({msg: "Gagal"});
+    }
+  } else {
+    res.json({msg: "Gagal"});
+  }
+})
 
 module.exports = router;
